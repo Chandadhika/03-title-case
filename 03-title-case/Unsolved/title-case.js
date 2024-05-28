dc1b1eb57e78fb682a9114ce6e7424de61db8460
ANarngAung <anarngaung2-17@gmail.com>
2024-05-29 05:11:53 +0630

    create script file

--- 03-title-case/Unsolved/title-case.js
+++ 03-title-case/Unsolved/title-case.js
@@ -1,3 +1,30 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function(str) {};
+
+
+
+
+
+
+
+var expect = chai.expect;
+
+describe('titleCase', function() {
+  it('should return `The Quick Brown Fox Jumped Over The Lazy Dog` when given `the quick brown fox jumped over the lazy dog`', function() {
+    var str = 'the quick brown fox jumped over the lazy dog';
+
+    var result = titleCase(str);
+
+    expect(result).to.eql('The Quick Brown Fox Jumped Over The Lazy Dog');
+  });
+
+  it('should return `A Christmas Carol` when given `a christmas carol`', function() {
+    var str = 'a christmas carol';
+
+    var result = titleCase(str);
+
+    expect(result).to.eql('A Christmas Carol');
+  });
+});
+
